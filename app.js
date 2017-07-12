fffd2d267ea0d922c1f1286ac407d492a7ccc786
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-07-11 23:05:31 -0500

    loop through the projectData array

--- app.js
+++ app.js
@@ -10,22 +10,20 @@ function Project(projectDataObject){
   this.repoLink = projectDataObject.repoLink;
   this.imgSrc = projectDataObject.imgSrc;
 }
-console.log(Project);
 
 Project.prototype.toHtml = function(){
   var $newProjectArticle = $('article.template').clone();
   $newProjectArticle.toggleClass('template');
-
   $newProjectArticle.find('.byline h2').text(this.name);
   $newProjectArticle.find('.projectTextBox p').text(this.description);
   $newProjectArticle.find('.completedDate').text(this.dateCompleted);
   $newProjectArticle.find('.collaborators').text(this.colaborators);
   $newProjectArticle.find('.projectLinks:first-child').text(this.name + ' home Page').attr('href',this.pageLink);
   $newProjectArticle.find('.projectLinks:last-child').text(this.name + ' repository').attr('href',this.repoLink);
-  $newProjectArticle.find('.projectPicture').attr('src','imgSrc');
-
+  $newProjectArticle.find('.projectPicture').attr('src',this.imgSrc);
 }
 
 projectData.forEach(function(item){
-  //run the toHtml function with each of the objects from the data
+  var newProject = new Project(item);
+  newProject.toHtml();
 });
