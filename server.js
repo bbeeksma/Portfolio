b40c0ad809a1d88f0f991721a50a8688667ff802
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-07-23 23:07:59 -0500

    free up app variable since I want to use in my wrapper

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 
 const express = require('express');
 const PORT = 3000;
-const app = express();
+const exp = express();
 
-app.use(express.static('./public'));
+exp.use(express.static('./public'));
 
-app.get('*', function(request, response){
+exp.get('*', function(request, response){
   response.status(404).send('404 Page not found');
 });
 
-app.listen(PORT, function() {
+exp.listen(PORT, function() {
   console.log('express is listening on port ' + PORT);
 });
