0edcbd90e3f1750722e5919b65aad07c7d06d116
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-07-23 23:22:01 -0500

    add iife wrapper

--- public/app.js
+++ public/app.js
@@ -1,67 +1,71 @@
 'use strict';
+var app = app || {};
 
+(function(module){
+  var page = {};
 
-var page = {};
-
-function Project(projectDataObject){
-  Object.keys(projectDataObject).forEach(key => this[key] = projectDataObject[key]);
-}
-
-Project.prototype.toHtml = () => {
-  var theTemplateScript = $('#article-template').html();
-  var theTemplate = Handlebars.compile(theTemplateScript);
-  var theCompiledHtml = theTemplate(this);
-  $('#portfolio').append(theCompiledHtml);
-}
-
-Project.buildProjects = (data) => {
-  data.forEach(function(item){
-    var newProject = new Project(item);
-    newProject.toHtml();
-  });
-}
-
-Project.initProjects = () => {
-  var eTag;
-  $.ajax({
-    type: 'HEAD',
-    url: 'data/projects.json',
-    complete: function(XMLHttpRequest){
-      eTag = XMLHttpRequest.getResponseHeader('ETag');
-    }
-  });
-  if (localStorage.projectData && localStorage.eTag === eTag) {
-    let data = JSON.parse(localStorage.projectData)
-    Project.buildProjects(data);
+  function Project(projectDataObject){
+    Object.keys(projectDataObject).forEach(key => this[key] = projectDataObject[key]);
   }
-  else{
-    $.getJSON('data/projects.json',function(data,message,xhr){
-      window.localStorage.eTag = xhr.getResponseHeader('ETag');
-      window.localStorage.projectData = JSON.stringify(data);
-      Project.buildProjects(data);
+
+  Project.prototype.toHtml = () => {
+    var theTemplateScript = $('#article-template').html();
+    var theTemplate = Handlebars.compile(theTemplateScript);
+    var theCompiledHtml = theTemplate(this);
+    $('#portfolio').append(theCompiledHtml);
+  }
+
+  Project.buildProjects = (data) => {
+    data.forEach(function(item){
+      var newProject = new Project(item);
+      newProject.toHtml();
     });
   }
-}
 
-page.siteNavTabs = () => {
-  $('.siteNav .tabItem a').on('click', function() {
-    $('.tabBox').hide();
-    var activeTab = $(this).data('tab');
-    var makeIdForSelection = '#' + activeTab;
-    $(makeIdForSelection).fadeIn();
-  });
+  Project.initProjects = () => {
+    var eTag;
+    $.ajax({
+      type: 'HEAD',
+      url: 'data/projects.json',
+      complete: function(XMLHttpRequest){
+        eTag = XMLHttpRequest.getResponseHeader('ETag');
+      }
+    });
+    if (localStorage.projectData && localStorage.eTag === eTag) {
+      let data = JSON.parse(localStorage.projectData)
+      Project.buildProjects(data);
+    }
+    else{
+      $.getJSON('data/projects.json',function(data,message,xhr){
+        window.localStorage.eTag = xhr.getResponseHeader('ETag');
+        window.localStorage.projectData = JSON.stringify(data);
+        Project.buildProjects(data);
+      });
+    }
+  }
 
-  $('.siteNav .tabItem a:first').click();
-}
+  page.siteNavTabs = () => {
+    $('.siteNav .tabItem a').on('click', function() {
+      $('.tabBox').hide();
+      var activeTab = $(this).data('tab');
+      var makeIdForSelection = '#' + activeTab;
+      $(makeIdForSelection).fadeIn();
+    });
 
-page.accordionButton = () => {
-  $('.tabBox').on('click','.nameButton',function(){
-    $(this).siblings('.expandedProject').toggleClass('isExpanded');
-  })
-}
+    $('.siteNav .tabItem a:first').click();
+  }
 
-page.initPage = () => {
-  page.siteNavTabs();
-  page.accordionButton();
-  page.initProjects();
-}
+  page.accordionButton = () => {
+    $('.tabBox').on('click','.nameButton',function(){
+      $(this).siblings('.expandedProject').toggleClass('isExpanded');
+    })
+  }
+
+  page.initPage = () => {
+    page.siteNavTabs();
+    page.accordionButton();
+    page.initProjects();
+  }
+  module.Project = Project;
+  module.page = page;
+}(app));
